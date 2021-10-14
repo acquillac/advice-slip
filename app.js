941f7aab140340fb48be3aeaac1532df5e32fc34
acquillac <acquillaclarke@live.ca>
2021-10-14 07:24:55 -0600

    search with keyword

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-document.getElementById('button').addEventListener('click', () => {
-    // grabbing a random peice of advice
+// grabbing a random peice of advice
+document.getElementById('button1').addEventListener('click', () => {
     fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
@@ -7,4 +7,25 @@ document.getElementById('button').addEventListener('click', () => {
         const advice = data.slip.advice;
         document.getElementById('output').innerHTML = `<li>${advice}</li>`;
     })    
+})
+
+
+// searching for advice with keyword
+document.getElementById('button2').addEventListener('click', () => {   
+
+    // getting search input
+    const input = document.getElementById('search-input').value;
+
+    // adding search to the url
+    const url = 'https://api.adviceslip.com/advice/search/' + input;
+    
+    // fetching url
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const advice = data.slips[0].advice;
+        document.getElementById('search-output').innerHTML = `<li>${advice}</li>`;
+    })
+    .catch(document.getElementById('search-output').innerHTML = 
+    `<li>Sorry We couldn't find a result for ${input}</li>`);    
 })
